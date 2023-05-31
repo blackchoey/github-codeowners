680d6d328d894b753e6f2bb6d106fc1c06109fd6
Chaoyi Yuan <blackchoey@gmail.com>
2023-05-31 11:08:56 +0800

    update

--- src/lib/ownership/OwnershipEngine.ts
+++ src/lib/ownership/OwnershipEngine.ts
@@ -47,6 +47,7 @@ export class OwnershipEngine {
 
       for (const line of lines) {
         console.log(`Processing ${line}`);
+        console.log(`ASIIC: ${stringToAsciiCodes(line)}`)
         if (!line || line.startsWith('#')) {
           console.log(`Skip`);
           continue;
@@ -106,3 +107,12 @@ const createMatcherCodeownersRule = (rule: string): FileOwnershipMatcher => {
 
 // ensures that only the following patterns are allowed @octocat @octocat/kitty docs@example.com
 const codeOwnerRegex = /(^@[a-zA-Z0-9_\-/]*$)|(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+
+function stringToAsciiCodes(str) {
+  const asciiCodes = [];
+  for (let i = 0; i < str.length; i++) {
+    asciiCodes.push(str.charCodeAt(i));
+  }
+  return asciiCodes;
+}
